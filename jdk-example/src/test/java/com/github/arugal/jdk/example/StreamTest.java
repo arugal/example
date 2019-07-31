9cb3e73d54696ff518af4d61fae880edf59ad248
zhangwei <pknfe@outlook.com>
2019-07-31 22:41:08 +0800

    add stream code

--- jdk-example/src/test/java/com/github/arugal/jdk/example/StreamTest.java
+++ jdk-example/src/test/java/com/github/arugal/jdk/example/StreamTest.java
@@ -3,6 +3,11 @@ package com.github.arugal.jdk.example;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Spliterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RecursiveTask;
+import java.util.stream.Stream;
 
 /**
  * @author zhangwei
@@ -11,24 +16,73 @@ public class StreamTest {
 
     @Test
     public void streamTest() {
-        int maxLength = Arrays.asList("A", "AA").stream()
+        int maxLength = Stream.of("A", "AA")
                 .filter(a -> a.startsWith("A"))
-                .mapToInt(a -> a.length())
+                .mapToInt(String::length)
                 .max().getAsInt();
         System.out.println(maxLength);
     }
 
     @Test
     public void findFirst() {
-        Integer first = Arrays.asList(1, 2, 3)
-                .stream()
-                .map(x -> {
-                    System.out.println(x);
-                    return x;
-                })
+        Integer first = Stream.of(1, 2, 3)
+                .peek(System.out::println)
                 .filter(x -> x >= 3)
                 .findFirst().get();
 
         System.out.println("first:" + first);
+    }
+
+    @Test
+    public void spliteratorTest() {
+        Spliterator<Integer> spliterator = Arrays.asList(1, 2, 3, 4)
+                .spliterator();
+
+        spliterator.forEachRemaining(System.out::println);
+    }
+
+
+    public static class ForkJoinSumCalculator extends RecursiveTask<Long> {
+
+        private static final long serialVersionUID = -1332593115901476964L;
+
+        private final long[] numbers;
+        private final int start;
+        private final int end;
+
+        private static final long THRESHOLD = 10_000;
+
+        public ForkJoinSumCalculator(long[] numbers) {
+            this(numbers, 0, numbers.length);
+        }
+
+        public ForkJoinSumCalculator(long[] numbers, int start, int end) {
+            this.numbers = numbers;
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        protected Long compute() {
+            int lenght = end - start;
+            if (lenght <= THRESHOLD) {
+                return computeSequentially();
+            }
+            ForkJoinSumCalculator leftTask = new ForkJoinSumCalculator(numbers, start, start + lenght / 2);
+            leftTask.fork();
+
+            ForkJoinSumCalculator rightTask = new ForkJoinSumCalculator(numbers, start + lenght / 2, end);
+            Long rightResult = rightTask.compute();
+            Long leftResult = leftTask.join();
+            return leftResult + rightResult;
+        }
+
+        private long computeSequentially() {
+            long sum = 0;
+            for (int i = start; i < end; i++) {
+                sum += numbers[i];
+            }
+            return sum;
+        }
     }
 }
